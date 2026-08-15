d6059bf58669167bf6463408a497a094e2cfe090
jordanthejet <morepencils@gmail.com>
2026-08-15 05:39:30 -0400

    test(sop): spawn the adapter listener through the attribution macro

--- src/main.rs
+++ src/main.rs
@@ -10498,7 +10498,7 @@ mod tests {
             },
         );
         let (tx, _rx) = tokio::sync::mpsc::channel(8);
-        let listener = tokio::spawn(async move {
+        let listener = ::zeroclaw_spawn::spawn!(async move {
             use zeroclaw_api::channel::Channel;
             let _ = channel.listen(tx).await;
         });
